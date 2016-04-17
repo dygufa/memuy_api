03c58b68385eab32f7e4c0bd6bae2c05f92d64b9
Rodrigo Araujo <dygufa@gmail.com>
2016-04-17 03:27:06 -0700

    Informing needed enviroment variables

--- api/memuy_api.js
+++ api/memuy_api.js
@@ -1,2 +1,9 @@
+/*
+Enviroment variable to be defined:
+
+- MONGO_URL: As it says... the mongo URL
+- PORT: The port where the application will be running
+*/
+
 require('./app/core/mongoose')
 require('./app/core/router')
