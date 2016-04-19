db8d3cdcea94874ad6ad8c260e825170af2d3a35
Rodrigo Araujo <dygufa@gmail.com>
2016-04-18 22:25:33 -0700

    Adding file controller

---
+++ api/app/controllers/file.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+    Key = mongoose.model("Key"),
+    File = mongoose.model("File"),
+    ObjectId = mongoose.Types.ObjectId
+
+exports.listFiles = function(req, res, next) {
+    
+}
+
+exports.addFile = function(req, res, next) {
+
+}
+
+exports.getFile = function(req, res, next) {
+
+}
+
+exports.updateFile = function(req, res, next) {
+
+}
+
+exports.deleteFile = function(req, res, next) {
+
+}
