0dfba895cf8e3ad0a0df0f33e51a76cbec199975
Rodrigo Araujo <dygufa@gmail.com>
2016-04-20 02:35:26 -0700

    Enabling auto timestamps

--- api/app/models/Key.js
+++ api/app/models/Key.js
@@ -3,11 +3,9 @@ var Schema   = mongoose.Schema;
 var File = require("./File");
 
 var KeySchema = new Schema({
-    word: String,
-    passcode : String,
-    local_server_adress: String,
-    device_id: String,
-    created_at: Date,
+    key: String,
     files: [mongoose.model('File').schema]
-});
+}, {
+    timestamps: true
+})
 mongoose.model('Key', KeySchema);
