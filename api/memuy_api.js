2f0d2457b995c772ae7274b4ee02198c5d5c9882
Rodrigo Araujo <dygufa@gmail.com>
2016-04-20 02:33:38 -0700

    Documenting NODE_ENV

--- api/memuy_api.js
+++ api/memuy_api.js
@@ -3,6 +3,7 @@ Enviroment variable to be defined:
 
 - MONGO_URL: As it says... the mongo URL
 - PORT: The port where the application will be running
+- NODE_ENV: The enviroment running the application (dev or production)
 */
 
 require('./app/core/mongoose')
