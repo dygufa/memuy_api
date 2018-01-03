2a1be2799c44716db4df802f1755255211fac475
Rodrigo Araujo <dygufa@gmail.com>
2018-01-03 02:13:39 -0300

    moving folder

--- src/app/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-import * as FileController from "./FileController";
-import * as RoomController from "./RoomController";
-import * as SocketController from "./SocketController";
-
-export { FileController, RoomController, SocketController };
--- src/app/models/index.ts
+++
@@ -1,4 +0,0 @@
-import FileModel from "./FileModel";
-import RoomModel from "./RoomModel";
-import WordModel from "./WordModel"; 
-export { FileModel, RoomModel, WordModel }; 
