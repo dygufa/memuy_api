99c2872e0dc8d69a50f79fec4d8329f69a7365d6
Rodrigo Araujo <dygufa@gmail.com>
2018-01-15 02:28:17 -0300

    improving extension detection

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -7,13 +7,25 @@ import { ManagedUpload } from "aws-sdk/clients/s3";
 const S3_BUCKET = process.env.S3_BUCKET;
 const s3 = new aws.S3();
 
+function fileExtension(file: Express.Multer.File) {
+    let extension = mime.extension(file.mimetype);
+    
+    if (!extension) {
+        const re = /(?:\.([^.]+))?$/;
+        extension = re.exec(file.originalname)![1];  
+    }
+
+    return extension;
+}
+
 export async function upload(file: Express.Multer.File) {
     if (!S3_BUCKET) {
         throw new Error("S3_BUCKET is not defined");
     }
     
     const filename = slug(file.originalname.replace(/\.[^/.]+$/, ""));
-    const extension = mime.extension(file.mimetype);
+    const extension = fileExtension(file);
+
     const newFilename = Date.now().toString() + '-' + filename + '.' + extension;
 
     const fileStream = fs.createReadStream(file.path);
