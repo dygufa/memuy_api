806ad1b3d93e706f6577deadbb2f6491ad4ad0cc
Rodrigo Araujo <dygufa@gmail.com>
2019-07-24 02:14:44 -0300

    chore: upgrade mongoose version

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -2,9 +2,7 @@ import * as mongoose from "mongoose";
 
 const MONGO_URL = process.env.MONGO_URL || "";
 
-mongoose.connect(MONGO_URL, {
-    useMongoClient: true
-});
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on("error", function (err) {
@@ -15,9 +13,9 @@ db.once("open", function callback() {
     console.info("MongoDB connection is established");
 });
 
-db.on("disconnected", function() {
+db.on("disconnected", function () {
     console.error("MongoDB disconnected!");
-    mongoose.connect(MONGO_URL, {server:{auto_reconnect:true}});
+    mongoose.connect(MONGO_URL, { server: { auto_reconnect: true } });
 });
 
 db.on("reconnected", function () {
