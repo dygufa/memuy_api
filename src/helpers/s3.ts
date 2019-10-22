7c97eac12efe2b5160f9ac77905e4f0ee3b853f2
Rodrigo Araujo <dygufa@gmail.com>
2019-10-21 22:47:06 -0300

    feat: implement s3 remove method

--- src/helpers/s3.ts
+++ src/helpers/s3.ts
@@ -2,17 +2,17 @@ import * as aws from "aws-sdk";
 import * as slug from "slug";
 import * as mime from "mime-types";
 import * as fs from "fs";
-import { ManagedUpload } from "aws-sdk/clients/s3";
+import { ManagedUpload, DeleteObjectOutput } from "aws-sdk/clients/s3";
 
 const S3_BUCKET = process.env.S3_BUCKET;
 const s3 = new aws.S3();
 
 function fileExtension(file: Express.Multer.File) {
     let extension = mime.extension(file.mimetype);
-    
+
     if (!extension) {
         const re = /(?:\.([^.]+))?$/;
-        extension = re.exec(file.originalname)![1];  
+        extension = re.exec(file.originalname)![1];
     }
 
     return extension;
@@ -22,7 +22,7 @@ export async function upload(file: Express.Multer.File) {
     if (!S3_BUCKET) {
         throw new Error("S3_BUCKET is not defined");
     }
-    
+
     const filename = slug(file.originalname.replace(/\.[^/.]+$/, ""));
     const extension = fileExtension(file);
 
@@ -37,6 +37,25 @@ export async function upload(file: Express.Multer.File) {
             ContentType: file.mimetype,
             ACL: 'public-read',
             Body: fileStream
+        }).send((err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        });
+    });
+
+}
+
+export async function remove(fileKey: string) {
+    if (!S3_BUCKET) {
+        throw new Error("S3_BUCKET is not defined");
+    }
+
+    return <Promise<DeleteObjectOutput>>new Promise((resolve, reject) => {
+        s3.deleteObject({
+            Bucket: S3_BUCKET,
+            Key: fileKey,
         }).send((err, data) => {
             if (err) {
                 return reject(err);
